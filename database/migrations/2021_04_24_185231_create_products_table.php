c0ada83c87e65b6d2c74165c65d9edf8ce92e902
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2021-05-05 19:35:00 +0100

    Update 2021_04_24_185231_create_products_table.php

--- database/migrations/2021_04_24_185231_create_products_table.php
+++ database/migrations/2021_04_24_185231_create_products_table.php
@@ -15,6 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('category_id');
             $table->string('name');
             $table->string('image');
             $table->bigInteger('price');
